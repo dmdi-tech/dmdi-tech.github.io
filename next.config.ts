622ea9b1f336efb2eb7e2720e504acf02b234de8
dmdi-tech <d59692816@gmail.com>
2025-07-31 16:25:37 -0400

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
+  
   output: 'export' 
 };
 
