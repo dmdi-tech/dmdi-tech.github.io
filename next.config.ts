abff0e75d3e6d4258529634c0048a394b909eebe
dmdi-tech <d59692816@gmail.com>
2025-07-31 12:27:35 -0400

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/portfolio',       
+  assetPrefix: '/portfolio/',
+  output: 'export',  
 };
 
 export default nextConfig;
