ff9683da8fa806af8c5a65448eb1d11c97432faa
dmdi-tech <d59692816@gmail.com>
2025-07-31 12:05:33 -0400

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-import type { NextConfig } from "next";
-
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
-  images: {
-    unoptimized: true
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
+  output: 'export', 
 };
 
-export default nextConfig;
+module.exports = nextConfig;
