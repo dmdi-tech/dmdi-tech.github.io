a50f0ba2ba553f115e6ef5eb3ae17c2f8ec99b82
dmdi-tech <d59692816@gmail.com>
2025-07-31 15:27:42 -0400

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production"
-
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
   output: 'export',  
 };
 
